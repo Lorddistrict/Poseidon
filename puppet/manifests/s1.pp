0d5d2bd038817e98f72caf05e590d0061305eb4d
adjikpo <arthur.djikpo@gmail.com>
2021-10-06 00:13:04 +0200

    add the php config

--- puppet/manifests/s1.pp
+++ puppet/manifests/s1.pp
@@ -3,4 +3,5 @@ node 's1'{
   $servername = 's1.infra'
   include apache
   include apache::vhosts
+  include php
  }
--- puppet/modules/php/manifests/php.pp
+++ puppet/modules/php/manifests/php.pp
@@ -1,7 +1,14 @@
 # Apache & PHP installation
 class php {
   package {
-    'php7.3':
+    'php7.4':
+      name => 'php'
       ensure => present
     }
+
+    service { 'php-service':
+    name    => 'php',
+    ensure  => running,
+    enable  => true,
+  }
 }
