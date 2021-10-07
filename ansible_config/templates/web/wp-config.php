7ca1275eb9463c7a5a79ac27e09c8682da2ddfbd
Lorddistrict <etienne.crespi@laposte.net>
2021-10-07 21:32:50 +0200

    [Update] rewrite path & add wp-config for wordpress

---
+++ ansible_config/templates/web/wp-config.php
@@ -0,0 +1,4 @@
+define('DB_NAME', '{{ wordpress_name }}' );
+define('DB_USER', '{{ wordpress_user }}');
+define('DB_PASSWORD', '{{ wordpress_password }}');
+define('DB_HOST', '{{ wordpress_host }}');
